8d6b3163f75efecfa8030df32042a862779580d7
Igor Lima <62576492+IgorLim4@users.noreply.github.com>
2024-07-10 22:14:02 -0300

    Aula 2
    Foco ainda maior na parte de JavaScript, adicionando opcoes de salvamento, adcionar e outras coisas relacionados a lista de atividade

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// bibliotecas e codigos de terceiros
+const formatador = (data) => {
+  return {
+    dia:{
+      numerico: dayjs(data).format('DD'),
+      semana: {
+        curto: dayjs(data).format('ddd'),
+        longo: dayjs(data).format('dddd'),
+      }
+    },
+    
+
+    mes: dayjs(data).format('MMMM'),
+
+    hora: dayjs(data).format('HH:mm')
+  }
+}
+
+formatador(new Date('2024-04-01'))
+
+
 // object
 const atividade = {
   nome: "Almoço",
@@ -6,7 +27,7 @@ const atividade = {
   
 }
 // lista, array, vetor []
-const atividades = [
+let atividades = [
   atividade,
   {
     nome: 'Academia em grupo',
@@ -20,32 +41,153 @@ const atividades = [
   }
 ]
 
+// atividades = []
+
+
+
+
 
 // arrow function
 const criarItemDeAtividade = (atividade) => {
   
-  let input = '<input type="checkbox" '
+  let input = `<input onchange="concluirAtividade(event)" value="${atividade.data}" type="checkbox" `
+
   
+
+
+
   if(atividade.finalizada) {
     input = input + 'checked'
   }
 
   input = input + '>'
   
+
+  const formatar = formatador(atividade.data);
+
+
   return `
   <section>
     <div>
       ${input}
       <span>${atividade.nome}</span>
-      <time>${atividade.data}</time>
+      <time>
+        ${formatar.dia.semana.longo},
+        dia ${formatar.dia.numerico}
+        de ${formatar.mes}
+        as ${formatar.hora}h
+      </time>
     </div>
   </section>
   `
 }
 
-const section = document.querySelector('section')
+const atualizarListaAtividades = () => {
+  const section = document.querySelector('section')
+  section.innerHTML = ''
 
-for(let atividade of atividades) {
-  section.innerHTML += criarItemDeAtividade(atividade)
+  // verificar se a lista esta vazia
+  if(atividades.length == 0){
+    section.innerHTML = '<p>Nenhuma atividade foi cadastrada.</p>'
+    return
+  }
+
+
+
+
+
+  for(let atividade of atividades) {
+    section.innerHTML += criarItemDeAtividade(atividade)
+  }
 }
 
+
+atualizarListaAtividades()
+
+
+
+const salvarAtividade = (event) => {
+  event.preventDefault()
+  const dadosDoFormulario = new FormData(event.target)
+
+  const nome = dadosDoFormulario.get('atividade')
+  const dia = dadosDoFormulario.get('data')
+  const hora = dadosDoFormulario.get('hora')
+  const data = `${dia} ${hora}`
+  
+  const novaAtividade = {
+    nome,
+    data,
+    finalizada: false
+  }
+
+  const atividadeExiste = atividades.find((atividade) => {
+    return atividade.data == novaAtividade.data
+  })
+
+  if(atividadeExiste)
+  {
+    return alert('Dia/Hora nao disponivel')
+  }
+
+  atividades = [novaAtividade, ...atividades]
+  atualizarListaAtividades()
+}
+
+const criarDiasSelecao = () => {
+  const dias =[
+    "2024-02-23",
+    "2024-05-18",
+    "2024-02-12",
+    "2024-01-30",
+    "2024-08-24",
+  ]
+
+  let diasSelecao = ''
+
+  for(let dia of dias) {
+    const formatar = formatador(dia)
+    const diaFormatado = `${formatar.dia.numerico} de ${formatar.mes}`
+
+    diasSelecao += `<option value="${dia}">${diaFormatado}</option>`
+    
+  }
+
+  
+  document.querySelector('select[name="data"').innerHTML = diasSelecao
+}
+
+criarDiasSelecao()
+
+
+const criarHorasSelecao = () => {
+  let horasDisponiveis = [];
+
+  for(let i = 6; i <= 22; i++) {
+    const hora = String(i).padStart(2,'0')
+    horasDisponiveis.push(`<option value="${hora}:00">${hora}:00</option>`);
+    horasDisponiveis.push(`<option value="${hora}:30">${hora}:30</option>`);
+  }
+
+  document.querySelector('select[name="hora"]').innerHTML = horasDisponiveis.join('');
+}
+
+criarHorasSelecao();
+
+
+const concluirAtividade = (event) => {
+  const input = event.target
+  const dataDesteInput = input.value
+
+  const atividade = atividades.find((atividade) => {
+    return atividade.data == dataDesteInput
+  })
+
+  if(!atividade) {
+    return
+  }
+
+
+  atividade.finalizada = !atividade.finalizada
+
+}
--- index.md
+++ index.md
@@ -1,14 +1,48 @@
-HTML
+**HTML**
+
+html{
+
+  background-color:#09090B;
+
+  color:#f4f4f5;
+
 - Hypertext
 - Markup
   - tags, elementos
 - Language
 
-CSS
+**CSS**
+
 - Cascading Style Sheet
 
-JavaScript
+**JavaScript**
+
 - Interpretada pelo browser
 - dinamica
 - orientado a objeto
 - ECMA Script trazendo inovacoes
+
+
+**Conceitos fundamentais de programacao**
+
+- Comentarios
+- Declaracao de variaveis (const. let)
+- Operadores (atribuicao, concatenacao, matematicos)
+- Tipos de dados (string, number, boolean, Date )
+- Estrutura de dados (functions, object, array)
+- Controle de fluxi (if, else)
+- Estrutura de repeticao (for)
+- Manipulacao de dados
+  - Conversao de dados
+
+
+Algoritmo
+
+- sequencia de passos logicos e finita para resolucao de um problema
+
+
+Fases da resolucao de um problema
+
+1. Coletar dados
+2. Processar os dados
+3. Apresentar os dados
